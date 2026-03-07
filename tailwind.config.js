1625a011a4e6b2cd9e0accb6a2c3fc1d76a2d256
officialsami007 <officialsami007@gmail.com>
2026-03-07 15:47:42 +0400

    added tailwind configuration

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        slate: {
+          900: '#0f172a',
+          800: '#1e293b',
+          700: '#334155',
+          600: '#475569',
+          400: '#cbd5e1',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
